f2ae7564bf27990ab8109f99e4af8fdfcfbfea76
Michael Kozlov <lordomarian@gmail.com>
2020-12-25 04:43:47 +0300

    Lesson21 Ключевое слово Final

---
+++ src/Lesson22.java
@@ -0,0 +1,11 @@
+public class Lesson22 {
+    public static void main (String[] args){
+        final int x = 10;
+        System.out.println(x);// константу можно вывести но нельзя изменить.
+    }
+}
+class Test{
+    public static final int CONSTANT= 0; // final используется для того чтобы создать константу
+   /* public Test(int CONSTANT){
+        this.CONSTANT = CONSTANT;
+    */}
